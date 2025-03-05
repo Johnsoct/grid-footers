8415f6a07319029c305d1458f06d10f51ccd118a
Chris Johnson <johnsoctbus@gmail.com>
2025-03-04 20:57:56 -0500

    WIP column flow

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,39 @@
 
 import "./style.scss"
 
-const buttonGridColumnFlow = document.getElementById("grid-column-row")
+const buttonGridColumnFlow = document.getElementById("grid-column-flow")
 const buttonGridRowFlow = document.getElementById("grid-row-flow")
 const sandboxEl = document.getElementById("sandbox")
+
+function generateGridFlowColumnColumns() {
+    for (let i = 1; i <= 40; i++) {
+        const column = document.createElement("div")
+        const fragment = new DocumentFragment()
+        const grid = document.createElement("div")
+        const label = document.createElement("label")
+
+        column.classList.add("flex-column")
+        grid.classList.add("grid", "grid-flow-column", "grid-template-columns", "grid-template-rows", "gap", "footer-column", `footer-column--${i}`, "border")
+        grid.id = "injecthere"
+        sandboxEl?.classList.add("flex", "flex-wrap", "gap")
+
+        label.textContent = `${i} items`
+
+        column.append(label, grid)
+
+        fragment.append(column)
+        
+        for (let ii = 1; ii <= i; ii++) {
+            const anchor = document.createElement("a")
+
+            anchor.textContent = "1st Class Boat and RV Storage"
+
+            fragment.getElementById("injecthere")?.appendChild(anchor)
+        }
+
+        sandboxEl?.appendChild(fragment)
+    }
+}
 
 function generateGridFlowRowColumns() {
     for (let i = 1; i <= 40; i++) {
@@ -38,5 +68,16 @@ function generateGridFlowRowColumns() {
     }
 }
 
-buttonGridRowFlow.addEventListener("click", generateGridFlowRowColumns)
+function removeGrids() {
+    sandboxEl.innerHTML = ``
+}
 
+buttonGridColumnFlow.addEventListener("click", () => {
+    removeGrids()
+    generateGridFlowColumnColumns()
+})
+
+buttonGridRowFlow.addEventListener("click", () => {
+    removeGrids()
+    generateGridFlowRowColumns()
+})
